c473b6971091390a8ca1548fb396ca99729ce551
gsuriya <79863266+gsuriya@users.noreply.github.com>
2026-05-20 18:54:58 -0400

    Add: redundant-connection - submission-7

---
+++ Data Structures & Algorithms/redundant-connection/submission-7.py
@@ -0,0 +1,47 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        """
+
+        for each edge
+        - check if u,v alr connected
+        - if yes --> add to removed else add to graph
+
+        return correct edge from removed list
+
+        """
+
+        # checks if u and v are connected alr
+        def dfs(node, target, visited):
+            if node == target:
+                return True
+            if node in visited:
+                return False
+            
+            visited.add(node)
+
+            # dfs on neighbors
+            for nei in adj_list[node]:
+                if dfs(nei, target, visited):
+                    return True
+            return False
+
+        removed = []
+        adj_list = defaultdict(list)
+        for u, v in edges:
+            # alr connected
+            if dfs(u, v, set()):
+                removed.append([u, v])
+            # NOT alr connected
+            else:
+                adj_list[u].append(v)
+                adj_list[v].append(u)
+        
+        # return last processed edge from EDGES that must be removed
+        return removed[-1] 
+        
+
+
+
+
+
+        
