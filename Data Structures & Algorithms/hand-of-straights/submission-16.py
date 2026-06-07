d3cb79816a472bb8633bc8136836f7b70d7a3733
gsuriya <79863266+gsuriya@users.noreply.github.com>
2026-06-06 21:46:45 -0400

    Add: hand-of-straights - submission-16

---
+++ Data Structures & Algorithms/hand-of-straights/submission-16.py
@@ -0,0 +1,55 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        """
+
+        put in hashmap
+        - go thru freq_map in sorted order using minheap / sorted keys
+
+        freq_map = {
+            1: 1
+            2: 2
+            3: 2
+            4: 2
+            5: 1
+        }
+
+        k = 5
+
+        1. put all unique keys in minheap
+        2. peek minheap
+        - start counting from there, decrementing consecutive nums in freq_map
+        - if freq[n] = 0 --> remove it from heap and map
+            - HOWEVER, this shld alw be the min. if we DON'T remove the min
+              we create a "hole" in the remaining numbers so they're NOT consecutive
+            - so return false if the n that is freq[n] = 0 is NOT the min in the minheap
+
+        """
+
+        # freq_map, put all keys in minheap
+        freq_map = defaultdict(int)
+        for c in hand:
+            freq_map[c] += 1
+        minh = list(freq_map.keys()) # faster to heapify O(N) rather than insert all keys in O(NlogN) where N is number of keys
+        heapq.heapify(minh)
+        
+        # start counting from min
+        while minh: # while numbers still remain
+            start = minh[0]
+
+            # count from start, decrement freq_map
+            for n in range(start, start+groupSize):
+                if n not in freq_map:
+                    return False
+                
+                freq_map[n] -= 1
+
+                # pop from heap if none of this n left
+                if freq_map[n] == 0:
+                    # if its not the min we're popping, hv to create hole to count consecutive --> return False
+                    if n != minh[0]:
+                        return False
+                    
+                    heapq.heappop(minh)
+
+        return True
+
