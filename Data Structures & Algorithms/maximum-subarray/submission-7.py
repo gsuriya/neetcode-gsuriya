3dd91e6911234b7568d694fe6257f8de08aaf718
gsuriya <79863266+gsuriya@users.noreply.github.com>
2026-06-06 21:12:54 -0400

    Add: maximum-subarray - submission-7

---
+++ Data Structures & Algorithms/maximum-subarray/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        """
+
+        2 -3 4 -2 2 1 -1 4
+
+        if curr_sum negative --> throw away cus it doesn't help at all
+
+        """
+        
+        max_sum = float("-inf")
+        curr_sum = 0
+        L = 0
+
+        for R in range(len(nums)):
+            # shrink - collapse window
+            if curr_sum < 0:
+                curr_sum = 0
+                L = R
+            
+            # expand
+            curr_sum += nums[R]
+
+            # update
+            max_sum = max(max_sum, curr_sum)
+        
+        return max_sum
+            
+
