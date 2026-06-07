abb2d381eeadd105c0f0d7febe7e8b3f886d759b
gsuriya <79863266+gsuriya@users.noreply.github.com>
2026-06-06 20:13:32 -0400

    Add: number-of-one-bits - submission-3

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        """
+
+        integer n can have log(n) bits for it
+
+        - going through all bits for 1s --> O(logN)
+
+
+        mask w/ 1 and keep shifting n right
+        """
+
+        count = 0
+        while n > 0:
+            if n & 1 == 1:
+                count += 1
+            n >>= 1
+        return count
