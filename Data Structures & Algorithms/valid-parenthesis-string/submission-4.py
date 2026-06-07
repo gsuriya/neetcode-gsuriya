fcfb80f67be9660d899e00375f9fe72269b2bcf4
gsuriya <79863266+gsuriya@users.noreply.github.com>
2026-06-06 20:06:28 -0400

    Add: valid-parenthesis-string - submission-4

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-4.py
@@ -0,0 +1,41 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        """
+
+        normally would hv only 1 net variable, but w/ * now we hv a RANGE OF POSSIBILITIES
+
+        net_min --> increment on "(", decrement on ")", BUT decrement on "*"
+        net_max --> increment on "(", decrement on ")", BUT increment on "*"
+
+        if all *** then net_min negative --> make sure net_min floor is 0
+        net_max can keep decreasing as more ))) and can become neg --> this var tracks ))( 
+
+        IMPORTANT RULES:
+        1. **** --> net_min floored to 0
+        2. ))(( --> net_max becoming negative 
+
+        """
+        # they act together normally, but w/ * then not
+        net_min, net_max = 0, 0
+
+        for c in s:
+            if c == "(":
+                net_min += 1
+                net_max += 1
+            elif c == ")":
+                net_min -= 1
+                net_max -= 1
+            elif c == "*":
+                net_min -= 1
+                net_max += 1
+            net_min = max(0, net_min)
+
+            if net_max < 0: return False # ))((
+        
+        return net_min == 0
+
+
+
+
+
+            
