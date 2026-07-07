136a0c453e9b222a00b1ae2e1129a94318d5d6aa
gsuriya <79863266+gsuriya@users.noreply.github.com>
2026-07-07 13:41:24 -0400

    Add: is-anagram - submission-60

---
+++ Data Structures & Algorithms/is-anagram/submission-60.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # create freq_maps for each and compare equality
+        s_map = defaultdict(int)
+        for c in s:
+            s_map[c] +=1 
+        
+        t_map = defaultdict(int)
+        for c in t:
+            t_map[c] += 1
+        
+        # compare equality
+        if len(s_map) != len(t_map):
+            return False
+        for key in s_map:
+            if s_map[key] != t_map.get(key, -1):
+                return False
+        return True
