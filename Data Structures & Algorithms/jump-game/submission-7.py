cf586973b408ccaee0553e495806b0eb3f5b56a8
gsuriya <79863266+gsuriya@users.noreply.github.com>
2026-07-27 13:28:30 -0400

    Add: jump-game - submission-7

---
+++ Data Structures & Algorithms/jump-game/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        """
+
+              i goal
+        1 2 0 1 0
+
+        """
+
+        goal = len(nums)-1
+        
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        
+        return goal == 0
