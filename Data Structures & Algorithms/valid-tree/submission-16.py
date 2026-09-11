48f2ddfe4d772583a4366c6c6895cac4b8f5c060
gsuriya <79863266+gsuriya@users.noreply.github.com>
2026-09-11 15:19:44 -0400

    Add: valid-tree - submission-16

---
+++ Data Structures & Algorithms/valid-tree/submission-16.py
@@ -0,0 +1,53 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        """
+
+        don't need visiting and visited, js visited cus dfs'ing
+        from only one node to check len(visited)
+
+        1. connected - can reach ALL nodes len(visited) = n
+        2. no cycles
+
+        """
+        if not edges:
+            return True
+
+        # create adj_list
+        adj_list = defaultdict(list)
+        for src, dst in edges:
+            adj_list[src].append(dst)
+            adj_list[dst].append(src)
+        # add lone nodes --> return False cus not connected
+        for node in range(n):
+            if node not in adj_list:
+                return False # lone node
+
+        # dfs from one node, check for cycles in UNDIRECTED graph
+        visited = set()
+        def dfs(node, parent):
+            if node in visited:
+                return True # cycle detected
+
+            visited.add(node)
+
+            # dfs on every nei except parent
+            for nei in adj_list[node]:
+                if nei == parent:
+                    continue
+                if dfs(nei, node): 
+                    return True
+            
+            return False
+
+        if dfs(0, -1):
+            return False # cycle found --> not valid tree
+        
+        if len(visited) != n: # not all nodes found from dfs --> not connected
+            return False
+        
+        return True
+        
+
+            
+
+
